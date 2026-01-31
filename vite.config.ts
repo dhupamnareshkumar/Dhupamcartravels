f7af9ad39d6333cf4a0508070e08a0b4a611a4a5
Dhupam <nareshkumar.dhupam@cdk.com>
2026-01-31 22:58:05 +0530

    modefied to create dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ✅ required for custom domain
+
   server: {
     host: "::",
     port: 8080,
@@ -12,10 +13,14 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean,
+  ),
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // ✅ REQUIRED
     },
   },
 }));
